55cf47b20a44ae30d012f543da8d8eb10aa94bb7
Rivera Ascencio <lriveraascencio@deloitte.com>
2024-01-03 13:51:50 -0600

    Exception test throw

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,7 +1,7 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -27,7 +27,12 @@ class IndexControllerTest {
 	@Test
 	@DisplayName("Test Exception")
 	void testOupsHandler() {
-		assertTrue("notimplemented".equals(controller.oupsHandler()),"this is some expensive " + "Message to build" + "for my test");
+		assertThrows(ValueNotFoundException.class, () -> {
+			controller.oupsHandler();
+		});
+		//assertTrue("notimplemented".equals(controller.oupsHandler()),"this is some expensive " + "Message to build" + "for my test");
 	}
+	
+	
 
 }
